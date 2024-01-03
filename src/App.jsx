2f5fe0730bbc055a791dbd64fda7ba4d83028950
Jacob Yang <jacobyang1738@gmail.com>
2024-01-03 11:54:35 -0600

    Index Changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,14 @@ const selectedWord = wordCollection[Math.floor(Math.random()*wordCollection.leng
 function App() {
 
   const [userIn, setUserIn] = useState('')
+  const [submitted0, setSubmitted0] = useState(false)
   const [submitted1, setSubmitted1] = useState(false)
   const [submitted2, setSubmitted2] = useState(false)
   const [submitted3, setSubmitted3] = useState(false)
   const [submitted4, setSubmitted4] = useState(false)
   const [submitted5, setSubmitted5] = useState(false)
   const [attempt, setAttempt] = useState(0);
-  const [attempts, setAttempts] = useState(['', '', '', '', ''])
+  const [attempts, setAttempts] = useState(['', '', '', '', '', ''])
   
   const check = (e) => {
     e.preventDefault()
@@ -40,18 +41,21 @@ function App() {
 
   function setActiveSubmit() {
     if(attempt === 0) {
-      setSubmitted1(true)
+      setSubmitted0(true)
     }
     else if(attempt === 1) {
-      setSubmitted2(true)
+      setSubmitted1(true)
     }
     else if(attempt === 2) {
-      setSubmitted3(true)
+      setSubmitted2(true)
     }
     else if(attempt === 3) {
-      setSubmitted4(true)
+      setSubmitted3(true)
     }
     else if(attempt === 4) {
+      setSubmitted4(true)
+    }
+    else if(attempt === 5) {
       setSubmitted5(true)
     }
   }
@@ -59,11 +63,12 @@ function App() {
   return (
     <div  className="body">
       <div className="input-display">
-        <Attempt className={attempt === 0 ? 'active' : 'inactive'} word={attempts[0]} selectedWord={selectedWord} submitted={submitted1} attempts={attempts} attempt={attempt}></Attempt>
-        <Attempt className={attempt === 1 ? 'active' : 'inactive'} word={attempts[1]} selectedWord={selectedWord} submitted={submitted2} attempts={attempts} attempt={attempt}></Attempt>
-        <Attempt className={attempt === 2 ? 'active' : 'inactive'} word={attempts[2]} selectedWord={selectedWord} submitted={submitted3} attempts={attempts} attempt={attempt}></Attempt>
-        <Attempt className={attempt === 3 ? 'active' : 'inactive'} word={attempts[3]} selectedWord={selectedWord} submitted={submitted4} attempts={attempts} attempt={attempt}></Attempt>
-        <Attempt className={attempt === 4 ? 'active' : 'inactive'} word={attempts[4]} selectedWord={selectedWord} submitted={submitted5} attempts={attempts} attempt={attempt}></Attempt>
+        <Attempt className={attempt === 0 ? 'active' : 'inactive'} word={attempts[0]} selectedWord={selectedWord} submitted={submitted0} attempts={attempts} attempt={attempt}></Attempt>
+        <Attempt className={attempt === 1 ? 'active' : 'inactive'} word={attempts[1]} selectedWord={selectedWord} submitted={submitted1} attempts={attempts} attempt={attempt}></Attempt>
+        <Attempt className={attempt === 2 ? 'active' : 'inactive'} word={attempts[2]} selectedWord={selectedWord} submitted={submitted2} attempts={attempts} attempt={attempt}></Attempt>
+        <Attempt className={attempt === 3 ? 'active' : 'inactive'} word={attempts[3]} selectedWord={selectedWord} submitted={submitted3} attempts={attempts} attempt={attempt}></Attempt>
+        <Attempt className={attempt === 4 ? 'active' : 'inactive'} word={attempts[4]} selectedWord={selectedWord} submitted={submitted4} attempts={attempts} attempt={attempt}></Attempt>
+        <Attempt className={attempt === 5 ? 'active' : 'inactive'} word={attempts[5]} selectedWord={selectedWord} submitted={submitted5} attempts={attempts} attempt={attempt}></Attempt>
       </div>
       <form onSubmit={check}>
         <input type="text"
